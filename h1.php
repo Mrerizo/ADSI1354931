4b759729c1d452cc57023a8d7de45466dbf16c28
jeferson <jefersonlozano07@gmail.com>
2017-11-21 10:38:16 -0500

    add tag img and tag h1

--- h1.php
+++ h1.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" type="estilo.css" href="">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
 </head>
 <body>
+	<div class="cuadro">
+		<h1>hola</h1>
+		<img src="https://www.w3.org/Style/Examples/011/captura15.png">
+	</div>
 
 </body>
 </html>
