2a0981154ed5102b5df1caa487c62069e8844044
jefersonguzman <jefersonlozano07@gmail.com>
2017-11-21 11:11:20 -0500

    add style

--- h1.php
+++ h1.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<link rel="stylesheet" type="estilo.css" href="">
+<link rel="stylesheet" type="text/css" href="estilo.css">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -10,8 +10,7 @@
 <body>
 	<div class="cuadro">
 		<h1>hola</h1>
-		<img src="https://www.w3.org/Style/Examples/011/captura15.png">
+<img src="https://www.w3.org/Style/Examples/011/captura15.png">
 	</div>
-
 </body>
 </html>
